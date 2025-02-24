14769c3c04eb6fd70ff4ea0c3a7820616f1853d0
Krzysztof Kalkowski <kkalkowski@pulsonline.pl>
2025-02-24 16:16:39 +0100

    Add redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,20 @@
 import styles from './Column.module.scss';
 import Card from './../Card/Card';
 import CardForm from './../CardForm/CardForm';
+import { useSelector } from 'react-redux';
+
 
 const Column = props => {
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
     return (
         <article className={styles.column}>
             <h2 className={styles.title}> <span className={styles.icon + ' fa fa-' + props.icon} />
             {props.title}</h2>
             <ul className={styles.cards}>
-	            {props.cards.map(card => <Card key={card.id} title={card.title} />)}
-                </ul>
-                <CardForm columnId={props.id} action={props.addCard} />
-                </article>
+                {cards.map(card => <Card key={card.id} title={card.title} />)}
+            </ul>
+            <CardForm columnId={props.id} action={props.addCard} />
+            </article>
     );
 };
 
